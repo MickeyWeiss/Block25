e6aead85460740ea553ea2ae25bb9b2a4ff05aa2
MickeyWeiss <167358315+MickeyWeiss@users.noreply.github.com>
2024-06-27 20:49:12 -0600

    Add files via upload
    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const Color = (props) => {
+  return <div className={props.color}></div>;
+};
+const App = () => {
+  return (
+    <div id="container">
+      <div id="navbar">
+        <div>Currently selected: </div>
+        <div className="red">red</div>
+      </div>
+      <div id="colors-list">{<div><Color color="yellow"/><Color color="green"/><Color color="black"/></div>}</div>
+    </div>
+  );
+};
+
+export default App;
